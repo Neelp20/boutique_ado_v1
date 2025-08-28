57f5938e21acf4090697f9d1132f1e76b3847fe2
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-08-28 11:27:18 +0100

    Added checkout views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})  # first will get the bag from the session
+    if not bag:  # if there is nothing in the bag
+        messages.error(request, "There's nothing in your bag at the moment")  # then add a simple error message
+        return redirect(reverse('products'))  # and redirect back to the product page,will prevent people from typing/checkout
+
+    order_form = OrderForm()  # create an instance of our order form, which will be empty for now
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
