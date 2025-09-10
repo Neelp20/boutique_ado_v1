2eecdebbe62ac7b111b3618498472d8de8777b7d
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-09-10 14:00:20 +0100

    Prepare project for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutiqueadov5.wsgi:application
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,14 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k4x*#!)dm@sjphnuind8!qb1eb^e7f$asne0t2gdk*fytoj5p('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',  # vs code preview
     'localhost',  # listen for stripe webhooks
+    'boutique-ado-v5-5ac1a7269de6.herokuapp.com',  # heroku application
 ]
 
 
@@ -130,12 +131,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
